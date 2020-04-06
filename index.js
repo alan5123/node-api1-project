941fba66d77ba3fa4456ef925fbf618eec5114d9
alan5123 <alanparker5123@gmail.com>
2020-04-06 16:16:19 -0400

    server running and first endpoint set up

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+
+const server = express()
+
+//middleware
+server.use(express.json()); //teaches the server to parse JSON from the body
+
+let users = [
+    {
+        id:1,
+        name: "Alan Parker"
+    },
+
+    {
+        id:2,
+        name: "Lesly Alvarenga"
+    }
+]
+server.get('/', (req, res) => {
+    res.json({ api: 'running.....'})
+})
+
+server.get("/api/users", (req, res) => {
+    res.json(users)
+})
+
+
+
+const port = 5000;
+server.listen(port, () => console.log(`api is running on port ${port}`))

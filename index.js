9f23cc99ab7e6fab1d1e0577dba6dc76d8032410
alan5123 <alanparker5123@gmail.com>
2020-04-06 16:21:37 -0400

    can find user by id

--- index.js
+++ index.js
@@ -26,6 +26,18 @@ server.get("/api/users", (req, res) => {
     res.json(users)
 })
 
+server.get("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    
+    const user = users.find((user) => user.id == id)
+
+    if (user){
+        res.status(200).json(user)
+    }else {
+        res.status(404).json({message: "User not found"})
+    }
+    
+})
 
 
 const port = 5000;
